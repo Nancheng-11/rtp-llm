c0cead4c66eaee59286ab274ee036fc90224cb91
yangchengjun <yangchengjun.ycj@alibaba-inc.com>
2025-09-09 17:42:53 +0800

    feature - adapt deepseek in model py

---
+++ rtp_llm/models_py/modules/mla/__init__.py
@@ -0,0 +1,8 @@
+from rtp_llm.models_py.modules.mla.flashinfer_mla import (
+    MlaFlashInferPrefillOp,
+    MlaFlashInferDecodeOp,
+    TrtV2PrefillAttentionOp,
+)
+
+from rtp_llm.models_py.modules.mla.rotary_emb import MlaRotaryEmbeddingOp
+from rtp_llm.models_py.modules.mla.mla_attention import DeepSeekV2Attention
